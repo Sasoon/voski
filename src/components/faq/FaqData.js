9fe1fb16243b3cc4943ffc300c59c3e2d4e0cfb5
Sasoon Sarkisian <sasoon.sarkisian@sydney.msf.org>
2024-01-22 21:57:57 +1100

    Added faqs, cleaned up homepage

---
+++ src/components/faq/FaqData.js
@@ -0,0 +1,293 @@
+export const instrumental = [
+  {
+    id: 'inst-1',
+    title: 'I’ve never played an instrument and have no music theory knowledge, is that a problem?',
+    answer:
+      'Prior knowledge and experience is not necessary. If you are passionate and ready to try your best, feel comfortable to apply no matter what your level.',
+  },
+  {
+    id: 'inst-2',
+    title: 'Are there any age restrictions?',
+    answer: 'All students from ages 5 and up are welcome.',
+  },
+  {
+    id: 'inst-3',
+    title: 'Where do we purchase an instrument, equipment and music? How will we know what to buy?',
+    answer:
+      'In your initial lesson, your teacher will determine and recommend the appropriate sized instrument for you. You will also be given a list of items to buy before your upcoming lesson.',
+  },
+  {
+    id: 'inst-4',
+    title: 'Will students have performance opportunities throughout the year?',
+    answer:
+      'Most definitely. Our music school organises bi-yearly student concerts and performance opportunities for well-prepared students of all levels, including beginners.',
+  },
+  {
+    id: 'inst-5',
+    title:
+      'Do you prepare and help students apply for AMEB/ABRSM/Trinty exams? What about orchestral auditions or Eisteidffords for more advanced students?',
+    answer:
+      'We have been successfully preparing students for the above exams, auditions and competitions for over a decade. It is important to note that participation in the above is entirely optional.',
+  },
+  {
+    id: 'inst-6',
+    title: 'Where are lessons and rehearsals held? Do you do home visits?',
+    answer: 'All classes are held on premises.',
+  },
+  {
+    id: 'inst-7',
+    title: 'What is the duration of each lesson?',
+    answer:
+      'Lessons are 30, 45 or 60 minutes long. The duration of each lesson is determined by the teacher and student.',
+  },
+  {
+    id: 'inst-8',
+    title: 'Can I change my lesson day and time?',
+    answer:
+      'You are automatically booked for the same day and time each week for the entirety of the school term. That is your personal lesson time and it isn’t subject to change until the following term. We have a small window during school holidays to make changes if possible.',
+  },
+  {
+    id: 'inst-9',
+    title: 'Can I book a one-off lesson?',
+    answer:
+      'Lessons are 30, 45 or 60 minutes long. The duration of each lesson is determined by the teacher and student.',
+  },
+  {
+    id: 'inst-10',
+    title: 'Can I book group learning sessions instead of individual classes?',
+    answer: 'Group learning sessions are in no way substitutes for individual lessons.',
+  },
+  {
+    id: 'inst-11',
+    title: 'Do you offer online lessons? When can we utilise this option?',
+    answer:
+      "Online lessons are always available and very useful when either teacher or student cannot attend their lesson in person for any reason.  Questions about invoices, fees, lesson specifics, cancellations, tardiness, purchasing instruments and music, concert information, or any other inquiries will be addressed in your initial lesson. Prior to enrollment, you will receive an email with a copy of the terms and conditions for you to review and retain for future use. If you have further questions, please don't hesitate to contact us here.",
+  },
+];
+
+export const musicianship = [
+  {
+    id: 'mus-1',
+    title: 'If a student has little to no music theory knowledge, is that an issue?',
+    answer:
+      'No, prior knowledge and experience is not necessary, and we’ll always start with the basics. If you are passionate and ready to try your best, feel comfortable to apply no matter what your level.',
+  },
+  {
+    id: 'mus-2',
+    title: 'Are there any age restrictions?',
+    answer: 'All students from ages 5 and up are welcome.',
+  },
+  {
+    id: 'mus-3',
+    title: 'Where are private and group theory lessons held?',
+    answer: 'All classes are held on premises.',
+  },
+  {
+    id: 'mus-4',
+    title: 'What do we bring to our lesson?',
+    answer: 'Students will get clear instructions on what to purchase for lessons.',
+  },
+  {
+    id: 'mus-5',
+    title: 'How long and frequent are the lessons?',
+    answer:
+      'Lesson duration depends on individual assessment of the student. Lessons are held weekly.',
+  },
+  {
+    id: 'mus-6',
+    title: 'Can we enroll in both group and individual musicianship lessons?',
+    answer: 'Yes.',
+  },
+  {
+    id: 'mus-7',
+    title: 'Can we book group musicianship lessons instead of individual musicianship lessons?',
+    answer:
+      'Yes! One-on-one classes are of course more effective and fast progressing, but group theory lessons are also effective and usually more enjoyable for students. Keep in mind that the school will organise students into groups, and these groups will probably change in the subsequent terms.',
+  },
+  {
+    id: 'mus-8',
+    title: 'Do you offer online and in person lessons?',
+    answer: 'Yes.',
+  },
+  {
+    id: 'mus-9',
+    title: 'Do you prepare and help students apply for state theory exams?',
+    answer:
+      'We have been successfully preparing students for the theory exams for over a decade. It is important to note that participation in the state theory exams are entirely optional.',
+  },
+  {
+    id: 'mus-10',
+    title: 'Can we change the musicianship lesson day and time?',
+    answer:
+      'You are automatically booked for the same day and time each week for the entirety of the school term. That is your personal lesson time and it isn’t subject to change until the following term. We have a small window during school holidays to make changes if needed.',
+  },
+  {
+    id: 'mus-11',
+    title: 'Can we book in a casual musicianship lesson, instead of weekly lessons?',
+    answer:
+      'Yes, however single, fortnightly and casual lessons are not available for the set prices. Enquire separately for these prices.',
+  },
+  {
+    id: 'mus-12',
+    title: 'How are musicianship lessons billed?',
+    answer:
+      'Musicianship lessons are billed in advance every term, like all other lessons. All details will be included on the invoice which will be emailed to you.',
+  },
+  {
+    id: 'mus-13',
+    title: 'Can we book lessons during the school holidays?',
+    answer:
+      'Lessons are available during holidays or on public holidays, please enquire if you’re interested.',
+  },
+];
+
+export const ensemble = [
+  {
+    id: 'ens-1',
+    title: 'What level does the student need to be to join an ensemble?',
+    answer:
+      "Ensembles are open to students playing at an intermediate or advanced level, your teacher will assess your level regularly and let you know when you're ready to join an ensemble.",
+  },
+  {
+    id: 'ens-2',
+    title: 'Where are rehearsals held?',
+    answer: 'All rehearsals are held on premises.',
+  },
+  {
+    id: 'ens-3',
+    title: 'Where do we purchase ensemble music notes?',
+    answer:
+      'The majority of our ensemble music is either acquired by the school or specially arranged by the ensembles teacher to suit the unique skills of our students. The expense and effort involved in arranging the music is provided as a complimentary service to our students.',
+  },
+  {
+    id: 'ens-4',
+    title: 'Will the school organise ensemble performances?',
+    answer: 'Yes! When the students are ready we will organise performing opportunities!',
+  },
+  {
+    id: 'ens-5',
+    title: 'What do we need to bring to each rehearsal?',
+    answer:
+      'It is the responsibility of the student to bring their instrument, all relevant music, pencil and eraser to the rehearsal each week. Music stands will be provided.',
+  },
+  {
+    id: 'ens-6',
+    title: 'What’s expected for homework?',
+    answer:
+      'Setting aside regular time to practice ensemble pieces is crucial, as successful ensemble rehearsals depend on a high level of teamwork, achievable only when each member diligently works on improving their individual parts.',
+  },
+  {
+    id: 'ens-7',
+    title: 'How are rehearsals billed?',
+    answer:
+      'Rehearsals are billed in advance every term. All details will be included on the invoice which will be emailed to you.',
+  },
+  {
+    id: 'ens-8',
+    title: 'Do you hold ensemble rehearsals during the holidays?',
+    answer: 'Yes, we offer this option!',
+  },
+  {
+    id: 'ens-9',
+    title: 'Can we book a casual lesson with our ensemble, if we aren’t enrolled in the school?',
+    answer:
+      'Yes, casual lessons are available for all ensembles. Please send details here for more information on availabilities and fees.',
+  },
+];
+
+export const groupIntroClass = [
+  {
+    id: 'gic-1',
+    title: 'Where are group learning sessions held?',
+    answer: 'All sessions are held on premises.',
+  },
+  {
+    id: 'gic-2',
+    title: 'How are group introductory classes billed?',
+    answer:
+      'You’ll be sent an invoice, and payment of the invoice will secure your spot in the session. No refunds or postponements.',
+  },
+  {
+    id: 'gic-3',
+    title: 'Who can book this class, who is it intended for?',
+    answer:
+      'Anyone and everyone! Enrolled students, potentially interested students, or maybe just looking for a one-off fun intro class with friends, everyone is welcome!',
+  },
+  {
+    id: 'gic-4',
+    title: 'How do we prepare for this class?',
+    answer:
+      'Arrive 5 minutes early so you don’t miss the beginning of the class, and we’ll take it from there!',
+  },
+];
+
+export const pianoAccompaniment = [
+  {
+    id: 'pa-1',
+    title: 'Where are rehearsals held?',
+    answer: 'All rehearsals are held on premises.',
+  },
+  {
+    id: 'pa-2',
+    title: 'How much time do we have during our piano rehearsals?',
+    answer:
+      'Depending on student’s level and amount of repertoire, your teacher will suggest a rehearsal length of either 45, 60 or 75 minutes. During this time, you will warm up, play through your pieces, and work on specific areas that need the most amount of attention.',
+  },
+  {
+    id: 'pa-3',
+    title: 'Can we change the piano rehearsal day and time?',
+    answer:
+      'After you have booked and paid for your time slot, there are no refunds or postponement options. You will need to cancel and wait for the next accompaniment session.',
+  },
+  {
+    id: 'pa-4',
+    title: 'Can we book a one-off lesson?',
+    answer: 'All piano accompaniment rehearsals are considered and billed as one-off lessons.',
+  },
+  {
+    id: 'pa-5',
+    title: 'How are these rehearsals billed?',
+    answer:
+      'All rehearsals and classes are billed through the school, we will send you an invoice and your payment will serve as your booking confirmation.',
+  },
+];
+
+export const groupLearningSessions = [
+  {
+    id: 'gls-1',
+    title: 'Where are group learning sessions held?',
+    answer: 'All sessions are held on premises.',
+  },
+  {
+    id: 'gls-2',
+    title:
+      'Are these sessions open to other students who would like to observe and not participate?',
+    answer: 'Yes! We highly encourage your presence, please contact us here.',
+  },
+  {
+    id: 'gls-3',
+    title: 'How are group learning sessions billed?',
+    answer:
+      'You’ll be sent an invoice, and payment of the invoice will secure your spot in the session. No refunds or postponements.',
+  },
+  {
+    id: 'gls-4',
+    title: 'Do you hold group learning sessions during the holidays?',
+    answer: 'Yes!',
+  },
+  {
+    id: 'gls-5',
+    title: 'Can we play in these sessions if we aren’t enrolled in the school?',
+    answer:
+      'Only students of the school can play in the sessions as spots are limited, however you are welcome to book and observe the sessions!',
+  },
+];
+
+export default {
+  instrumental,
+  musicianship,
+  ensemble,
+  groupIntroClass,
+  pianoAccompaniment,
+  groupLearningSessions,
+};
