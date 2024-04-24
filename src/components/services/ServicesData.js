0593336b8d3fc90ae7c07c84d10963d9221d4dc9
Sasoon Sarkisian <sasoon.sarkisian@sydney.msf.org>
2024-04-24 23:03:04 +1000

    Updated dependencies, address and content

--- src/components/services/ServicesData.js
+++ src/components/services/ServicesData.js
@@ -3,9 +3,18 @@ export const services = [
     name: 'Instrument Tuition',
     description:
       'One-on-one instrumental lessons providing personalised attention, leading to more effective and faster progress.',
-    img: `https://ucarecdn.com/63a53f12-c89f-4c23-836a-207025f065e6/-/resize/1920x/-/quality/smart_retina/-/format/auto/`,
-    thumbnail: `https://ucarecdn.com/63a53f12-c89f-4c23-836a-207025f065e6/-/preview/300x300/-/format/auto/-/quality/smart_retina/`,
+    img: 'https://ucarecdn.com/63a53f12-c89f-4c23-836a-207025f065e6/-/resize/1920x/-/quality/smart_retina/-/format/auto/',
+    thumbnail:
+      'https://ucarecdn.com/63a53f12-c89f-4c23-836a-207025f065e6/-/preview/300x300/-/format/auto/-/quality/smart_retina/',
     content: `Specialised instrumental lessons are valuable because they're tailored just for you. This personalised environment makes it comfortable to ask questions and grasp the tricky parts at your own pace. Throughout your learning journey, you'll receive ongoing feedback to ensure you're on the right track and making the most out of your lessons.`,
+    pricing: `
+      <strong>Specialised lessons</strong><br>
+      <i>(one on one lessons incorporating tailored instrumental tuition, musicianship, theory, music history and aural training)</i><br><br>
+      <i>$97.00 / 60 minutes</i><br>
+      <i>$122.00 / 75 minutes</i><br>
+      <br>
+      Fees are per student, per lesson.<br>
+      These lessons are booked and billed as a package.`,
   },
   {
     name: 'Group Introductory Class',
@@ -14,6 +23,12 @@ export const services = [
     img: `https://ucarecdn.com/9db9ad24-fe1f-4696-a49f-93663722c772/-/resize/1920x/-/quality/smart_retina/-/format/auto/`,
     thumbnail: `https://ucarecdn.com/9db9ad24-fe1f-4696-a49f-93663722c772/-/preview/300x300/-/format/auto/-/quality/smart_retina/`,
     content: `This is a fun, one-off group lesson for beginners of all ages. It's suitable whether you're enrolled in our music school, planning to enroll, or just want to learn about musical elements. The class covers various musical fundamentals in an engaging group setting.`,
+    pricing: `
+      <strong>Group Introductory Class</strong><br>
+      <i>(one-off trial class for all children aged 4-6)</i><br><br>
+      <i>$30.00 / 30 minutes</i><br>
+      <br>
+      This class is booked and billed as a one-off session.`,
   },
   {
     name: 'Musicianship',
@@ -22,6 +37,13 @@ export const services = [
     img: `https://ucarecdn.com/b2f35ea7-36af-4076-a9a9-ef947988dc88/-/resize/1920x/-/quality/smart_retina/-/format/auto/`,
     thumbnail: `https://ucarecdn.com/b2f35ea7-36af-4076-a9a9-ef947988dc88/-/preview/300x300/-/format/auto/-/quality/smart_retina/`,
     content: `Musicianship is about becoming a musician, not just learning to play an instrument technically. It involves observing and listening in a musical context, reading scores, ear and rhythm training, music theory, composing basics, and a light dip into music history.`,
+    pricing: `
+      <strong>Musicianship Class</strong><br>
+      <i>(includes musicianship, composition, music history, theory, aural training, and improvisation)</i><br><br>
+      <i>$55.00 / 60 minutes</i><br>
+      <br>
+      Fees are per student, per class.<br>
+      These classes are booked and billed as a package.`,
   },
   {
     name: 'Piano Accompaniment',
@@ -32,6 +54,14 @@ export const services = [
     thumbnail: `https://ucarecdn.com/
 70d77066-9cfc-4724-b8e9-1d670f59ed66/-/preview/300x300/-/format/auto/-/quality/smart_retina/`,
     content: `Piano accompaniment rehearsals are practice sessions with a pianist playing an accompanying part. They develop students' listening skills and interpretation, simulating live performance conditions, building confidence, and enhancing both technical and expressive performance aspects.`,
+    pricing: `
+      <strong>Piano Accompaniment</strong><br>
+      <i>(lesson and rehearsal with a collaborative pianist)</i><br><br>
+      <i>$48.00 / 30 minutes</i><br>
+      <i>$60.00 / 45 minutes</i><br>
+      <br>
+      Fees are per student, per class.<br>
+      This rehearsal is booked and billed as a one-off session.`,
   },
   {
     name: 'Open Masterclass',
@@ -39,6 +69,13 @@ export const services = [
     img: `https://ucarecdn.com/36c5aa41-e516-4b65-8747-ee46699e4bad/-/resize/1920x/-/quality/smart_retina/-/format/auto/`,
     thumbnail: `https://ucarecdn.com/36c5aa41-e516-4b65-8747-ee46699e4bad/-/preview/300x300/-/format/auto/-/quality/smart_retina/`,
     content: `Intermediate and advanced level students are eligible to participate in our masterclass sessions. If you would like to play in an open masterclass session, speak to your teacher. Check here regularly for any upcoming masterclasses. Our open masterclasses are music workshops where a teacher offers high-level instruction and insights. Observers can listen, learn valuable skills, and ask questions at the end of the class.`,
+    pricing: `
+      <strong>Open Masterclass</strong><br>
+      <i>(class with an experienced professional musician in front of a small audience)</i><br><br>
+      <i>$60.00 (to participate)</i><br>
+      <i>$10.00 (to observe)</i><br>
+      <br>
+      This class is booked and billed as a one-off session.`,
   },
   {
     name: 'Ensembles',
@@ -46,6 +83,13 @@ export const services = [
     img: `https://ucarecdn.com/13050525-68b4-4091-a833-195c2aaa0569/-/resize/1920x/-/quality/smart_retina/-/format/auto/`,
     thumbnail: `https://ucarecdn.com/13050525-68b4-4091-a833-195c2aaa0569/-/preview/300x300/-/format/auto/-/quality/smart_retina/`,
     content: `Ensemble lessons involve a group of musicians playing as a team, guided by a teacher or conductor. They offer a fulfilling experience that motivates continued learning and improvement, fostering personal growth and enhancing musical skills in an exciting educational environment.`,
+    pricing: `
+      <strong>Ensembles</strong><br>
+      <i>(small groups of musicians rehearsing ensemble music with a teacher)</i><br><br>
+      <i>$30.00 / 45 minutes</i><br>
+      <br>
+      Fees are per student, per lesson.<br>
+      These lessons are booked and billed as a package.`,
   },
 ];
 
