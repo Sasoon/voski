c2df78c87e796713e7958053752f74f3fb1f94bc
Sasoon Sarkisian <sasoon.sarkisian@sydney.msf.org>
2024-04-25 10:05:35 +1000

    Added dynamic sitemap and robots.txt

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,11 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import compress from 'astro-compress';
 
+import sitemap from '@astrojs/sitemap';
+
 // https://astro.build/config
 export default defineConfig({
   experimental: {},
-  integrations: [tailwind(), compress()],
+  site: 'https://voskimusicstudio.com.au',
+  integrations: [tailwind(), compress(), sitemap()],
 });
